8d985867dd7d9d6a7a3fe70250982fae4f23ba7a
NicksPatties <nickspatties@gmail.com>
2022-10-03 13:58:52 -0700

    Deleted irrelevant tests.

--- e2e/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-  await page.goto('/');
-  expect(await page.textContent('h1')).toBe('Welcome to default value');
-  // flaky and I hate it
-  await page.waitForTimeout(1000); // greater than mock return time
-  expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
-});
