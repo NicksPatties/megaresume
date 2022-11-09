54e832aedcc2dded95f48a8d1470a7a20c1de532
NicksPatties <nickspatties@gmail.com>
2022-11-09 12:30:46 -0700

    Change textarea size to 4 columns, add delete button to highlights, add hide button that doesn't do anything yet to highlights.

--- src/components/workMenu.svelte
+++ src/components/workMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Input from '@src/components/input.svelte';
+  import Textarea from '@src/components/textarea.svelte';
   import AddEntryButton from '@src/components/addEntryButton.svelte';
   import type { Writable } from 'svelte/store';
   import { onArrayInput } from '@src/util/eventListeners';
@@ -11,6 +12,17 @@
   export let endDate: Writable<string>;
   export let summary: Writable<string>;
   export let highlights: Writable<Array<string>>;
+
+  function removeHighlight(i: number) {
+    highlights.update((h) => {
+      h.splice(i, 1);
+      return h;
+    });
+  }
+
+  function hideHighlight(i: number) {
+    console.log(`hide highlight at i=${i}, but doesn't do anything yet...`);
+  }
 </script>
 
 <Input label={'Name'} value={name} />
@@ -19,16 +31,32 @@
 <Input label={'End Date'} value={endDate} />
 <Input label={'Summary'} value={summary} />
 
-<label for="location">Highlights</label>
 {#each $highlights as highlight, i}
-  <textarea
-    id="highlight[{i}]"
-    placeholder="A cool highlight"
-    rows="3"
-    on:input={(e) => {
-      onArrayInput(e, highlights, i);
-    }}>{highlight}</textarea
-  >
+  <label for="highlight[{i}]">
+    Highlight {i + 1}
+    <div class="label-controls">
+      <button
+        on:click={() => {
+          hideHighlight(i);
+        }}>Hide</button
+      >
+      <button
+        on:click={() => {
+          removeHighlight(i);
+        }}>Delete</button
+      >
+    </div>
+  </label>
+  <Textarea
+    id={`highlight[${i}]`}
+    placeholder={'A cool highlight'}
+    content={highlight}
+    oninput={(e) => {
+      if (e != null) {
+        onArrayInput(e, highlights, i);
+      }
+    }}
+  />
 {/each}
 <AddEntryButton
   text={'Add new highlight'}
@@ -43,14 +71,7 @@
 
 <style>
   label {
-    display: block;
-  }
-
-  textarea {
-    margin-top: 8px;
-    font-family: serif;
-    font-size: 16px;
-    resize: none;
-    width: 100%;
+    display: flex;
+    justify-content: space-between;
   }
 </style>
