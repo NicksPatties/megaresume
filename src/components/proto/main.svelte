9e7a4976fff889ebaf8eec8941ab5ee3327baa1e
NicksPatties <nickspatties@gmail.com>
2022-12-01 11:07:06 -0700

    Move menu prototype to this project

---
+++ src/components/proto/main.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<main>
+  <h1>thing</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla pharetra diam sit amet nisl suscipit. Amet risus nullam eget felis eget. Nibh ipsum consequat nisl vel pretium. Ac feugiat sed lectus vestibulum. Nisi lacus sed viverra tellus in. Et magnis dis parturient montes nascetur ridiculus mus. Varius morbi enim nunc faucibus a pellentesque sit amet porttitor. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Tincidunt dui ut ornare lectus sit. Libero nunc consequat interdum varius. Lorem sed risus ultricies tristique. Leo vel orci porta non pulvinar neque laoreet suspendisse. Dignissim enim sit amet venenatis urna cursus eget nunc. Vulputate mi sit amet mauris commodo.</p>
+  
+  <h2>Sub thing</h2>
+
+  <p>Elementum facilisis leo vel fringilla est. Sit amet nisl suscipit adipiscing bibendum est. Sodales neque sodales ut etiam. Egestas fringilla phasellus faucibus scelerisque eleifend. Lorem sed risus ultricies tristique nulla aliquet. Velit egestas dui id ornare. Risus in hendrerit gravida rutrum quisque non tellus. Ut sem viverra aliquet eget. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Velit euismod in pellentesque massa. Pretium aenean pharetra magna ac placerat. Mi quis hendrerit dolor magna eget est lorem ipsum dolor.</p>
+  
+  <h2>Another sub thing</h2>
+
+  <p>Elementum facilisis leo vel fringilla est. Sit amet nisl suscipit adipiscing bibendum est. Sodales neque sodales ut etiam. Egestas fringilla phasellus faucibus scelerisque eleifend. Lorem sed risus ultricies tristique nulla aliquet. Velit egestas dui id ornare. Risus in hendrerit gravida rutrum quisque non tellus. Ut sem viverra aliquet eget. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Velit euismod in pellentesque massa. Pretium aenean pharetra magna ac placerat. Mi quis hendrerit dolor magna eget est lorem ipsum dolor.</p>
+</main>
+
+<style>
+  /**
+    mock document stuff
+  **/
+  main {
+    max-width: 600px;
+    margin: 0 auto;
+    font-size: 18px;
+    padding-bottom: 32px;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+</style>
---
+++ src/components/proto/menu.svelte
@@ -0,0 +1,172 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const openButton = document.getElementById('open-button')!;
+    const backButton = document.getElementById('back-button')!;
+    const menu = document.getElementById('menu')!;
+    openButton.onclick = () => {
+      menu.style.display = "";
+    }
+    backButton.onclick = () => {
+      menu.style.display = "none";
+    }
+  })
+</script>
+
+<div id="menu-component">
+  <button id="open-button">Open</button>
+  <div id="menu" class="menu">
+    <header id="menu-header">
+      <button id="back-button">Close</button>
+      <h2 class="menu-title">Menu Test</h2>
+    </header>
+    <div class="menu-contents" id="text-menu-contents">
+      <h1>What if I the text right now?</h1>
+      <p>Putting text inside of the menu may be a cool thing when you're adding a some documentation into the whatever the funk I dunno. It doesn't need to be inside a menu group at all so that's nice.</p>
+      <h2>Making the blungy</h2>
+      <p>You can just do the whatever and that's fine. Lorem ipsum is kinda boring so it's fun to make dumb shit myself!</p>
+      <h3>Next step</h3>
+      <p>Do the thing</p>
+      <h4>Next step</h4>
+      <p>Do the thing</p>
+      <h5>Next step</h5>
+      <p>Do the thing</p>
+      <h6>I am baby</h6>
+      <p>Yes you are</p>
+      <div class="divider"></div>
+      <ul class="menu-group" id="inputs-menu-contents">
+        <li>Option 1</li>
+        <li>Option 2</li>
+        <li>Option 3</li>
+      </ul>
+      <div class="divider"></div>
+      <ul class="menu-group">
+        <li>Option 4</li>
+        <li>Option 5</li>
+        <li>Option 6</li>
+        <li>Option 7</li>
+        <div class="divider"></div>
+        <li>Option 8</li>
+        <li>Option 9</li>
+        <li>Option 10</li>
+        <li>Option 11</li>
+        <li>Option 12</li>
+      </ul>
+      <footer>
+        <p><i>Made with 💨 by NicksPatties</i></p>
+      </footer>
+    </div> <!-- end menu-contents -->
+  </div> <!-- end menu -->
+</div>
+
+<style>
+  * {
+    box-sizing: object-fit;
+  }
+
+  :root {
+    --mobile-width: 400px;
+    --menu-height: 100vh;
+    --header-height: 42px;
+    --menu-box-shadow-width: 12px;
+    --divider-color: gray;
+    --menu-contents-text-side-padding: 7%;
+  }
+
+  /**
+    Menu component styles
+  **/
+  .menu {
+    background-color: white;
+    box-shadow: var(--divider-color) 0 0 var(--menu-box-shadow-width);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: var(--mobile-width);
+    height: var(--menu-height);
+    font-size: 18px;
+    font-family: sans-serif;
+    user-select: none;
+  }
+
+  /**
+    Menu header content
+  **/
+  #open-button {
+    position: fixed;
+    z-index: 0;
+    top: 10px;
+    left: 10px;
+  }
+
+  .menu header {
+    display: flex;
+    justify-content: center;
+    height: var(--header-height);
+    box-shadow: var(--divider-color) 0 0 9px;
+    clip-path: inset(0 0 -9px 0);
+  }
+
+  #back-button {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
+
+  .menu-title {
+    margin: auto 0;
+    text-align: center;
+  }
+
+  /**
+    Menu contents... content
+  **/
+  .menu-contents {
+    overflow-y: scroll;
+    height: calc(var(--menu-height) - var(--header-height));
+  }
+
+  .menu-contents > :is(h1, h2, h3, h4, h5, h6, p) {
+    padding: 0 var(--menu-contents-text-side-padding);
+  }
+
+  .menu-group {
+    margin: 0;
+    padding: 0;
+  }
+
+  .menu-group > li {
+    list-style: none;
+    font-size: 1em;
+    padding: 0.5em 0 0.5em var(--menu-contents-text-side-padding);
+  }
+
+  .menu-group li:hover {
+    background-color: red;
+  }
+
+  .menu-group li:active {
+    background-color: yellow;
+  }
+
+  .divider {
+    width: 80%;
+    height: 1px;
+    background: linear-gradient(90deg, white, #bababa, #bababa, white);
+    margin: 1em auto;
+  }
+
+  footer p {
+    font-size: 0.75em;
+    text-align: center;
+  }
+
+
+  @media only screen and (max-width: 400px) { /* should use var but whatever */
+    .menu {
+      box-shadow: none;
+      width: 100%;
+    }
+  }
+</style>
