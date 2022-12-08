cfbb2544c554ae1460b3998a169fdf9831fabf9c
NicksPatties <nickspatties@gmail.com>
2022-12-08 10:28:01 -0800

    Add opening and closing menu animation.

--- src/components/proto/menu.svelte
+++ src/components/proto/menu.svelte
@@ -1,151 +1,145 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  let menuOpen = false;
 
-  onMount(() => {
-    const openButton = document.getElementById('open-button');
-    const backButton = document.getElementById('back-button');
-    const menu = document.getElementById('menu');
-    if (openButton != null) {
-      openButton.onclick = () => {
-        if (menu != null) menu.style.display = '';
-      };
-    }
+  const openMenu = () => {
+    menuOpen = true;
+  };
 
-    if (backButton != null) {
-      backButton.onclick = () => {
-        if (menu != null) menu.style.display = 'none';
-      };
-    }
-  });
+  const closeMenu = () => {
+    menuOpen = false;
+  };
 
   const hide = () => {
     const currMenu = document.getElementById('menu-contents-0');
     const subMenu = document.getElementById('menu-contents-1');
-    if(currMenu != null && subMenu != null) {
+    if (currMenu != null && subMenu != null) {
       currMenu.classList.remove('visible');
       subMenu.classList.add('visible');
     }
-  }
+  };
 
   const hideAgain = () => {
     const currMenu = document.getElementById('menu-contents-0');
     const subMenu = document.getElementById('menu-contents-1');
-    if(currMenu != null && subMenu != null) {
+    if (currMenu != null && subMenu != null) {
       subMenu.classList.remove('visible');
       currMenu.classList.add('visible');
     }
-  }
+  };
 </script>
 
 <div id="menu-component">
-  <button id="open-button">Open</button>
-  <div id="menu" class="menu">
+  <button id="open-button" on:click={openMenu}>Open</button>
+  <div id="menu" class="menu {menuOpen ? 'open' : ''}">
     <header id="menu-header">
-      <button id="back-button">Close</button>
+      <button id="back-button" on:click={closeMenu}>Close</button>
       <h2 class="menu-title">Menu Test</h2>
     </header>
-    <div id="menu-contents-0" class="menu-contents visible">
-      <input type="button" value="Go to submenu" on:click={hide}/>
-      <div class="divider"></div>
-      <h1>What if I the text right now?</h1>
-      <p>
-        Putting text inside of the menu may be a cool thing when you're adding a some documentation
-        into the whatever the funk I dunno. It doesn't need to be inside a menu group at all so
-        that's nice.
-      </p>
-      <h2>Making the blungy</h2>
-      <p>
-        You can just do the whatever and that's fine. Lorem ipsum is kinda boring so it's fun to
-        make dumb shit myself!
-      </p>
-      <h3>Next step</h3>
-      <p>Do the thing</p>
-      <h4>Next step</h4>
-      <p>Do the thing</p>
-      <h5>Next step</h5>
-      <p>Do the thing</p>
-      <h6>I am baby</h6>
-      <p>Yes you are</p>
-      <div class="divider" />
-      <!-- form elements can exist outside of another class, which may be easier -->
-      <fieldset>
-        <legend>Text box inputs</legend>
-        <label for="input-text">Text input</label>
-        <input id="input-text" type="text" placeholder="Type your text here" />
-        <label for="email-input"
-          >Email input <span class="hint">(i.e. sample@email.com)</span></label
-        >
-        <input id="email-input" type="email" placeholder="sample@email.com" />
-        <label for="url-input"
-          >Url input <span class="hint">(kinda like https://google.com)</span></label
-        >
-        <input id="url-input" type="url" />
-        <label for="phone-input">Phone number input</label>
-        <input id="phone-input" type="tel" />
-        <label for="date-input">Date input</label>
-        <input id="date-input" type="date" />
-        <label for="month-input">Month input</label>
-        <input id="month-input" type="month" />
-        <label for="textarea-input">Textarea input</label>
-        <textarea id="textarea-input" rows="4" />
-        <label for="select-input">Select input</label>
-        <select id="select-input" value="0">
-          <option value="0" disabled>Pick one of the following</option>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-          <option value="3">Option 3</option>
-        </select>
-        <label for="text-list-input"
-          >Text list input <span class="hint">(Select from the list or type your own)</span></label
-        >
-        <input id="text-list-input" type="text" list="text-list" />
-        <datalist id="text-list">
-          <option value="one">Option 1</option>
-          <option value="two">Option 2</option>
-          <option value="three">Option 3</option>
-        </datalist>
-      </fieldset>
+    <div class="menu-contents-container">
+      <div id="menu-contents-0" class="menu-contents visible">
+        <input type="button" value="Go to submenu" on:click={hide} />
+        <div class="divider" />
+        <h1>What if I the text right now?</h1>
+        <p>
+          Putting text inside of the menu may be a cool thing when you're adding a some
+          documentation into the whatever the funk I dunno. It doesn't need to be inside a menu
+          group at all so that's nice.
+        </p>
+        <h2>Making the blungy</h2>
+        <p>
+          You can just do the whatever and that's fine. Lorem ipsum is kinda boring so it's fun to
+          make dumb shit myself!
+        </p>
+        <h3>Next step</h3>
+        <p>Do the thing</p>
+        <h4>Next step</h4>
+        <p>Do the thing</p>
+        <h5>Next step</h5>
+        <p>Do the thing</p>
+        <h6>I am baby</h6>
+        <p>Yes you are</p>
+        <div class="divider" />
+        <!-- form elements can exist outside of another class, which may be easier -->
+        <fieldset>
+          <legend>Text box inputs</legend>
+          <label for="input-text">Text input</label>
+          <input id="input-text" type="text" placeholder="Type your text here" />
+          <label for="email-input"
+            >Email input <span class="hint">(i.e. sample@email.com)</span></label
+          >
+          <input id="email-input" type="email" placeholder="sample@email.com" />
+          <label for="url-input"
+            >Url input <span class="hint">(kinda like https://google.com)</span></label
+          >
+          <input id="url-input" type="url" />
+          <label for="phone-input">Phone number input</label>
+          <input id="phone-input" type="tel" />
+          <label for="date-input">Date input</label>
+          <input id="date-input" type="date" />
+          <label for="month-input">Month input</label>
+          <input id="month-input" type="month" />
+          <label for="textarea-input">Textarea input</label>
+          <textarea id="textarea-input" rows="4" />
+          <label for="select-input">Select input</label>
+          <select id="select-input" value="0">
+            <option value="0" disabled>Pick one of the following</option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </select>
+          <label for="text-list-input"
+            >Text list input <span class="hint">(Select from the list or type your own)</span
+            ></label
+          >
+          <input id="text-list-input" type="text" list="text-list" />
+          <datalist id="text-list">
+            <option value="one">Option 1</option>
+            <option value="two">Option 2</option>
+            <option value="three">Option 3</option>
+          </datalist>
+        </fieldset>
 
-      <fieldset>
-        <!-- May not actually use a legend for this since I want to hide the contents of the rest of the field set...-->
-        <legend>Inline input items</legend>
-        <div class="inline-input">
-          <label for="checkbox-input">Checkbox</label>
-          <input id="checkbox-input" type="checkbox" />
-        </div>
-        <div class="inline-input">
-          <label for="radio-input-group-1">Radio 1</label>
-          <input id="radio-input-group-1" type="radio" value="1" name="radio" />
-        </div>
-        <div class="inline-input">
-          <label for="radio-input-group-2">Radio 2</label>
-          <input id="radio-input-group-2" type="radio" value="2" name="radio" />
-        </div>
-      </fieldset>
+        <fieldset>
+          <!-- May not actually use a legend for this since I want to hide the contents of the rest of the field set...-->
+          <legend>Inline input items</legend>
+          <div class="inline-input">
+            <label for="checkbox-input">Checkbox</label>
+            <input id="checkbox-input" type="checkbox" />
+          </div>
+          <div class="inline-input">
+            <label for="radio-input-group-1">Radio 1</label>
+            <input id="radio-input-group-1" type="radio" value="1" name="radio" />
+          </div>
+          <div class="inline-input">
+            <label for="radio-input-group-2">Radio 2</label>
+            <input id="radio-input-group-2" type="radio" value="2" name="radio" />
+          </div>
+        </fieldset>
 
-      <fieldset>
-        <legend>Button inputs</legend>
-        <!-- Honestly would probably just use a button and hide this input -->
-        <div class="inline-input">
-          <label for="file-input">File input</label>
-          <input id="file-input" type="file" />
-        </div>
-        <div class="inline-input">
-          <label for="color-input">Color input</label>
-          <input id="color-input" type="color" />
-        </div>
-        <label for="button-input">Button input</label>
-        <input id="button-input" type="button" value="Click me!" />
-      </fieldset>
-      <div class="divider" />
-      <footer>
-        <p><i>Made with 💨 by NicksPatties</i></p>
-      </footer>
-    </div>
-    <!-- end menu-contents -->
-    <div id="menu-contents-1" class="menu-contents">
-      <h1>I am the submenu</h1>
-      <input type="button" value="Go back" on:click={hideAgain} />
+        <fieldset>
+          <legend>Button inputs</legend>
+          <!-- Honestly would probably just use a button and hide this input -->
+          <div class="inline-input">
+            <label for="file-input">File input</label>
+            <input id="file-input" type="file" />
+          </div>
+          <div class="inline-input">
+            <label for="color-input">Color input</label>
+            <input id="color-input" type="color" />
+          </div>
+          <label for="button-input">Button input</label>
+          <input id="button-input" type="button" value="Click me!" />
+        </fieldset>
+        <div class="divider" />
+        <footer>
+          <p><i>Made with 💨 by NicksPatties</i></p>
+        </footer>
+      </div>
+
+      <div id="menu-contents-1" class="menu-contents">
+        <h1>I am the submenu</h1>
+        <input type="button" value="Go back" on:click={hideAgain} />
+      </div>
     </div>
   </div>
   <!-- end menu -->
@@ -164,6 +158,8 @@
     --menu-box-shadow-width: 12px;
     --divider-color: gray;
     --menu-contents-text-side-padding: 6%;
+    --menu-transition-time: 0.35s;
+    --menu-transition-page-curve: cubic-bezier(0.22, 0.61, 0.36, 1);
   }
 
   /**
@@ -171,15 +167,21 @@
   **/
   .menu {
     background-color: white;
-    box-shadow: var(--divider-color) 0 0 var(--menu-box-shadow-width);
+    box-shadow: none;
     position: fixed;
     top: 0;
-    left: 0;
+    left: calc(-1 * var(--mobile-width));
     width: var(--mobile-width);
     height: var(--menu-height);
     font-size: 18px;
     font-family: sans-serif;
     user-select: none;
+    transition: left var(--menu-transition-time) var(--menu-transition-page-curve);
+  }
+
+  .menu.open {
+    left: 0;
+    box-shadow: var(--divider-color) 0 0 var(--menu-box-shadow-width);
   }
 
   /**
@@ -211,13 +213,17 @@
     text-align: center;
   }
 
+  .menu-contents-container {
+    height: calc(var(--menu-height) - var(--header-height));
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+
   /**
     Menu contents... content
   **/
   .menu-contents {
     display: none;
-    overflow-y: scroll;
-    height: calc(var(--menu-height) - var(--header-height));
     flex-direction: column;
   }
 
