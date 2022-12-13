6e0c2228f286e88d45f9cd6b3201e48f4401698e
NicksPatties <nickspatties@gmail.com>
2022-12-13 09:19:33 -0800

    Save point: animating hover points in between list items

--- src/components/proto/dragNDrop.svelte
+++ src/components/proto/dragNDrop.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { arrayMove } from '@src/util/arrayMove';
   let items = ['list 1', 'list 2', 'list 3'];
+  let hoveringIndex = -1;
 
   function itemDragStart(e: DragEvent | null, dropI: number) {
     if (e != null && e.dataTransfer != null) {
@@ -10,9 +11,10 @@
     }
   }
 
-  function dropZoneDragEnter(e: DragEvent | null) {
+  function dropZoneDragEnter(e: DragEvent | null, i: number) {
     if (e != null && e.dataTransfer != null) {
       e.preventDefault();
+      hoveringIndex = i;
     }
     console.log('dragZoneDragEneter');
   }
@@ -26,6 +28,7 @@
 
   function dropZoneDragLeave() {
     // DOM cleanup can happen in here, since this event will always happen
+    hoveringIndex = -1;
     console.log('dragZoneDragLeave');
   }
 
@@ -41,8 +44,8 @@
 
 {#each items as item, i}
   <li
-    class="drop-zone"
-    on:dragenter={dropZoneDragEnter}
+    class="drop-zone {hoveringIndex == i ? "hovering": ""}"
+    on:dragenter={(e) => dropZoneDragEnter(e, i)}
     on:dragover={dropZoneDragOver}
     on:dragleave={dropZoneDragLeave}
     on:drop={(e) => dropZoneDrop(e, i)}
@@ -54,8 +57,8 @@
   </li>
 {/each}
 <li
-  class="drop-zone"
-  on:dragenter={dropZoneDragEnter}
+  class="drop-zone {hoveringIndex == items.length ? "hovering": ""}"
+  on:dragenter={(e) => dropZoneDragEnter(e, items.length)}
   on:dragover={dropZoneDragOver}
   on:dragleave={dropZoneDragLeave}
   on:drop={(e) => dropZoneDrop(e, items.length)}
@@ -65,6 +68,17 @@
 
 <style>
   .drop-zone {
-    background: pink;
+    background: #ddd;
+    list-style: none;
+    height: 5px;
+    padding: 0;
+    transition: "background-color" 0.3s ease-in-out;
+    overflow: hidden;
+  }
+
+  .drop-zone.hovering{
+    background: #bbb;
+    padding: 0.25em;
+    height: 2em;
   }
 </style>
