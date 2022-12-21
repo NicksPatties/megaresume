33724704e48c2e103d59c3263635ecb7af7efb39
NicksPatties <nickspatties@gmail.com>
2022-12-21 16:30:10 -0700

    Move changing styles back into their respective components

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -102,3 +102,67 @@
     </div>
   </div>
 </div>
+
+<style>
+  #open-button {
+    position: fixed;
+    z-index: 0;
+    top: 10px;
+    left: 10px;
+  }
+
+  .menu {
+    background-color: white;
+    box-shadow: none;
+    position: fixed;
+    top: 0;
+    left: calc(-1 * var(--mobile-width));
+    width: var(--mobile-width);
+    height: var(--menu-height);
+    font-size: 18px;
+    font-family: sans-serif;
+    user-select: none;
+    transition: left var(--menu-transition-time) var(--menu-transition-page-curve);
+    z-index: 10;
+  }
+
+  .menu.open {
+    left: 0;
+    box-shadow: var(--divider-color) 0 0 var(--menu-box-shadow-width);
+  }
+
+  .menu header {
+    display: flex;
+    justify-content: center;
+    height: var(--header-height);
+  }
+
+  #back-button {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
+
+  .menu-title {
+    margin: auto 0;
+    text-align: center;
+  }
+
+  .menu-contents-container {
+    box-sizing: border-box;
+    position: relative;
+    height: calc(var(--menu-height) - var(--header-height));
+    overflow-x: hidden;
+  }
+
+  @media only screen and (max-width: 400px) {
+    /* should use var but whatever */
+    .menu {
+      width: 100%;
+    }
+
+    .menu.open {
+      box-shadow: none;
+    }
+  }
+</style>
--- src/components/menuContents.svelte
+++ src/components/menuContents.svelte
@@ -6,3 +6,24 @@
 <div {id} class="menu-contents {visible ? 'visible' : ''}">
   <slot />
 </div>
+
+<style>
+  .menu-contents {
+    box-sizing: border-box;
+    background: white;
+    min-height: calc(100vh - var(--header-height));
+    padding: 1em;
+    position: absolute;
+    top: 0;
+    left: calc(-1 * var(--mobile-width));
+    width: var(--mobile-width);
+    display: flex;
+    flex-direction: column;
+    overflow-y: hidden;
+    transition: left var(--menu-transition-time) var(--menu-transition-page-curve);
+  }
+
+  .menu-contents.visible {
+    left: 0;
+  }
+</style>
