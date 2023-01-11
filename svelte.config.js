9c098de3008bae12ee07f0bab34e413ac275a6cb
NicksPatties <nickspatties@gmail.com>
2023-01-10 16:39:45 -0800

    Deploy site (#31)
    * Create static.yml
    
    Add static yml file to support automatically deploying this page to github pages
    
    * update sveltekit packages, update config to support SPA application, add nojekyll file

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +10,15 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '200.html'
+    }),
+    prerender: { entries: [] },
     alias: {
       '@src': 'src'
+    },
+    paths: {
+      base: dev ? '' : '/megaresume'
     }
   }
 };
