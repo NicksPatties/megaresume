aff5e6e58f6ca0c490af9787006dcbad447c4be9
NicksPatties <nickspatties@gmail.com>
2023-01-17 16:08:04 -0700

    Fix updating fields for workResumeEntry. Simplified the imports of the WorkResumeEntry component.

--- src/components/resume/workResumeEntry.svelte
+++ src/components/resume/workResumeEntry.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
-  import type { Highlight, WorkStore } from '@src/data/data';
-  import { derived, get, type Writable } from 'svelte/store';
+  import type { WorkStore } from '@src/data/data';
+  import { derived, get } from 'svelte/store';
   import { tagsStore } from '@src/data/tag';
   import { isHighlightVisible, dateInputToDecoratedString } from '@src/util/resumeUtils';
 
   export let workStore: WorkStore;
+  let name = derived(workStore.name, ($n) => ($n == '' ? 'Name' : $n));
+  let position = derived(workStore.position, ($p) => ($p == '' ? 'Position' : $p));
+  let startDate = derived(workStore.startDate, ($sd) =>
+    dateInputToDecoratedString($sd, 'Start date')
+  );
+  let endDate = derived(workStore.endDate, ($ed) => dateInputToDecoratedString($ed, 'End date'));
 </script>
 
 <li>
-  <span class:placeholder={false}>{get(workStore.position)}</span>,
-  <span class:placeholder={false}>{get(workStore.name)}</span>
-  <i
-    ><span class:placeholder={false}>{get(workStore.startDate)}</span> -
-    <span class:placeholder={false}>{get(workStore.endDate)}</span></i
-  >
+  <span class:placeholder={$position == 'Position'}>{$position}</span>,
+  <span class:placeholder={$name == 'Name'}>{$name}</span>
+  <i>
+    <span class:placeholder={$startDate == 'Start date'}>{$startDate}</span> -
+    <span class:placeholder={$endDate == 'End date'}>{$endDate}</span>
+  </i>
 </li>
 <ul>
   {#if get(workStore.highlights).length > 0}
