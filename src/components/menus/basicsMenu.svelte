4478ab85c9a1ed86549ba0a17b42e92695bfa323
NicksPatties <nickspatties@gmail.com>
2023-01-23 14:03:04 -0800

    Remove Input component and eventListeners file. Updated basicsMenu.

--- src/components/menus/basicsMenu.svelte
+++ src/components/menus/basicsMenu.svelte
@@ -1,14 +1,53 @@
 <script lang="ts">
   import MenuContents from '@src/components/menuContents.svelte';
-  import Input from '@src/components/input.svelte';
-  import { basicsStore } from '@src/data/data';
-  let basics = basicsStore;
+  import { basicsStore, saveResumeDataToLocalStorage } from '@src/data/data';
+  import type { Writable } from 'svelte/store';
+
+  let name = basicsStore.name;
+  let label = basicsStore.label;
+  let phone = basicsStore.phone;
+  let email = basicsStore.email;
+
+  function onInput(e: Event, s: Writable<string>, saveData = saveResumeDataToLocalStorage) {
+    const target = e.target as HTMLInputElement;
+    if (target) {
+      s.set(target.value);
+      saveData();
+    }
+  }
 </script>
 
 <MenuContents id="menu-resume-basics">
   <h2>Basic Information</h2>
-  <Input id={'basics_name'} label={'Name'} value={basics.name} />
-  <Input id={'basics_title'} label={'Title'} value={basics.label} />
-  <Input id={'basics_phone'} label={'Phone'} value={basics.phone} />
-  <Input id={'basics_email'} label={'Email'} value={basics.email} />
+  <label for="basics_name">Name</label>
+  <input
+    id="basics_name"
+    type="text"
+    value={$name}
+    on:input={(e) => onInput(e, basicsStore.name)}
+  />
+
+  <label for="basics_title">Title</label>
+  <input
+    id="basics_title"
+    type="text"
+    value={$label}
+    on:input={(e) => onInput(e, basicsStore.label)}
+  />
+
+  <label for="basics_phone">Phone</label>
+  <input
+    id="basics_phone"
+    type="text"
+    value={$phone}
+    on:input={(e) => onInput(e, basicsStore.phone)}
+  />
+
+  <label for={'basics_email'}>Email</label>
+  <input
+    id="basics_email"
+    type="text"
+    value={$email}
+    on:input={(e) => onInput(e, basicsStore.email)}
+  />
 </MenuContents>
