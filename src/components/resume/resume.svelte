a76a016d7dde29035a5e801392b338fc1bed35bb
NicksPatties <nickspatties@gmail.com>
2023-04-07 14:09:45 -0600

    Write e2e test to verify the resume component is the correct size

--- src/components/resume/resume.svelte
+++ src/components/resume/resume.svelte
@@ -62,7 +62,7 @@
 </script>
 
 <div class="resume-container">
-  <div class="resume">
+  <div class="resume" data-testid="resume">
     <div class="basics">
       <p class="name" class:placeholder={$name.length == 0}>{$name ? $name : 'Your name'}</p>
       <p class="subname" class:placeholder={$label.length == 0}>
--- tests/components/resume.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from 'vitest';
-
-describe('Resume component tests', () => {
-  it('placeholder', () => {
-    expect(true).toBe(true);
-  });
-});
