b78ff682b06f3d5f6e63447c2bbacf4f41374f95
NicksPatties <nickspatties@gmail.com>
2023-11-08 14:38:16 -0800

    Update package.json and pnpm-lock.yaml file. Update packages to latest versions. Update prettier, eslint, and typescript configurations.

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run build && npm run preview',
-    port: 4174
+    port: 4173
   }
 };
 
