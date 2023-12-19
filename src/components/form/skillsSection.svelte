ccc44a9e1c8fff1079d3d1ab01cca8bedccd926f
NicksPatties <nickspatties@gmail.com>
2023-12-19 10:51:36 -0800

    Support lower case searches in skills

--- src/components/form/skillsSection.svelte
+++ src/components/form/skillsSection.svelte
@@ -5,7 +5,7 @@
 
   let searchTerm = writable('');
   let filteredTags = derived([tagsStore, searchTerm], ([$tagsStore, $searchTerm]) =>
-    $tagsStore.filter((tag) => tag.name.includes($searchTerm))
+    $tagsStore.filter((tag) => tag.name.toLowerCase().includes($searchTerm))
   );
 
   function updateSearchTerm(e: Event) {
